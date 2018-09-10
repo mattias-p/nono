7cef2e61fbd7b92688ff6656367d49270c337522
Mattias Päivärinta <mattias@paivarinta.se>
2018-09-10 21:33:29 +0200

    Use pest

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use std::num::ParseIntError;
-use std::str::FromStr;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
 
-struct Clue {
-    inner: Vec<usize>,
-}
+use pest::iterators::Pair;
+use pest::Parser;
 
-impl FromStr for Clue {
-    type Err = ParseIntError;
+#[derive(Parser)]
+#[grammar = "nono.pest"]
+struct NonoParser;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let inner = s
-            .split(' ')
-            .map(|n| usize::from_str_radix(n, 10))
-            .collect::<Result<Vec<_>, _>>()?;
-        Ok(Clue { inner })
+struct Clue(Vec<usize>);
+
+impl<'a> From<Pair<'a, Rule>> for Clue {
+    fn from(pair: Pair<Rule>) -> Self {
+        assert_eq!(pair.as_rule(), Rule::clue);
+        Clue(
+            pair.into_inner()
+                .map(|n| usize::from_str_radix(n.as_str(), 10).unwrap())
+                .collect::<Vec<_>>(),
+        )
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let clue_pairs = NonoParser::parse(Rule::clue, "1 2 3").unwrap_or_else(|e| panic!("{}", e));
+    for clue_pair in clue_pairs {
+        let clue = Clue::from(clue_pair.clone());
+        println!("{:?}", clue.0);
+    }
 }
