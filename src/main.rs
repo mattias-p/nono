be9ef3071663a9cd53fb22965bc92ec877df5862
Mattias Päivärinta <mattias@paivarinta.se>
2018-09-10 20:02:15 +0200

    Clue parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+struct Clue {
+    inner: Vec<usize>,
+}
+
+impl FromStr for Clue {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let inner = s
+            .split(' ')
+            .map(|n| usize::from_str_radix(n, 10))
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Clue { inner })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
