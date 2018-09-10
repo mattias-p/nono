bc48a7316d24112328dc16bf50886e38b8a5f415
Mattias Päivärinta <mattias@paivarinta.se>
2018-09-10 22:33:41 +0200

    Grid

--- src/main.rs
+++ src/main.rs
@@ -55,10 +55,84 @@ impl fmt::Display for ClueList {
     }
 }
 
+enum Cell {
+    Filled,
+    Crossed,
+    Undecided,
+    Impossible,
+}
+
+impl<'a> From<Pair<'a, Rule>> for Cell {
+    fn from(pair: Pair<Rule>) -> Self {
+        assert_eq!(pair.as_rule(), Rule::cell);
+        match pair.into_inner().next().unwrap().as_rule() {
+            Rule::filled => Cell::Filled,
+            Rule::crossed => Cell::Crossed,
+            Rule::undecided => Cell::Undecided,
+            Rule::impossible => Cell::Impossible,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Cell::Filled => write!(f, "#"),
+            Cell::Crossed => write!(f, "X"),
+            Cell::Undecided => write!(f, "."),
+            Cell::Impossible => write!(f, "!"),
+        }
+    }
+}
+
+struct GridLine(Vec<Cell>);
+
+impl<'a> From<Pair<'a, Rule>> for GridLine {
+    fn from(pair: Pair<Rule>) -> Self {
+        assert_eq!(pair.as_rule(), Rule::grid_line);
+        GridLine(pair.into_inner().map(Cell::from).collect())
+    }
+}
+
+impl fmt::Display for GridLine {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for cell in &self.0 {
+            write!(f, "{}", cell)?;
+        }
+        Ok(())
+    }
+}
+
+struct Grid(Vec<GridLine>);
+
+impl<'a> From<Pair<'a, Rule>> for Grid {
+    fn from(pair: Pair<Rule>) -> Self {
+        assert_eq!(pair.as_rule(), Rule::grid);
+        Grid(pair.into_inner().map(GridLine::from).collect())
+    }
+}
+
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let (first, rest) = self.0.split_first().unwrap();
+        write!(f, "{}", first)?;
+        for grid_line in rest {
+            write!(f, ",{}", grid_line)?;
+        }
+        Ok(())
+    }
+}
+
 fn main() {
     let pairs = NonoParser::parse(Rule::clue_list, "1 2 3,4 5").unwrap_or_else(|e| panic!("{}", e));
     for pair in pairs {
-        let clue = ClueList::from(pair.clone());
+        let clue = ClueList::from(pair);
         println!("{}", clue);
+    }
+    let pairs =
+        NonoParser::parse(Rule::grid, "X..##..X,.X..X.").unwrap_or_else(|e| panic!("{}", e));
+    for pair in pairs {
+        println!("{}", Grid::from(pair));
     }
 }
