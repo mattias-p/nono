19026733cf735da9d84c2f4de8408162fef23510
Mattias Päivärinta <mattias@paivarinta.se>
2018-09-13 21:28:18 +0200

    Transpose (and bilateral Freedom2)

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod parser;
 
 use fixedbitset::FixedBitSet;
 use parser::Cell;
+use parser::Clue;
 use parser::ClueList;
 use parser::GridLine;
 use parser::NonoParser;
@@ -16,18 +17,58 @@ use std::fmt;
 use std::io;
 use std::io::BufRead;
 
+struct Clues {
+    vert: ClueList,
+    horz: ClueList,
+}
+
+impl Clues {
+    fn horz(&self, transposed: bool) -> &[Clue] {
+        if transposed {
+            self.vert.0.as_slice()
+        } else {
+            self.horz.0.as_slice()
+        }
+    }
+    fn vert(&self, transposed: bool) -> &[Clue] {
+        self.horz(!transposed)
+    }
+    fn width(&self, transposed: bool) -> usize {
+        if transposed {
+            self.horz.0.len()
+        } else {
+            self.vert.0.len()
+        }
+    }
+    fn height(&self, transposed: bool) -> usize {
+        self.width(!transposed)
+    }
+    fn max_horz_len(&self, transposed: bool) -> usize {
+        let clue_list = if transposed { &self.vert } else { &self.horz };
+        clue_list.0.iter().map(|clue| clue.0.len()).max().unwrap()
+    }
+    fn max_vert_len(&self, transposed: bool) -> usize {
+        self.max_horz_len(!transposed)
+    }
+}
+
 struct Grid {
     width: usize,
+    height: usize,
     filled: FixedBitSet,
     crossed: FixedBitSet,
 }
 
 impl Grid {
-    fn index(&self, x: usize, y: usize) -> usize {
-        y * self.width + x
+    fn index(&self, x: usize, y: usize, transposed: bool) -> usize {
+        if transposed {
+            x * self.height + y
+        } else {
+            y * self.width + x
+        }
     }
-    fn get(&self, x: usize, y: usize) -> Cell {
-        let i = self.index(x, y);
+    fn get(&self, x: usize, y: usize, transposed: bool) -> Cell {
+        let i = self.index(x, y, transposed);
         match (self.filled.contains(i), self.crossed.contains(i)) {
             (false, false) => Cell::Undecided,
             (false, true) => Cell::Crossed,
@@ -35,31 +76,34 @@ impl Grid {
             (true, true) => Cell::Impossible,
         }
     }
-    fn fill(&mut self, x: usize, y: usize) {
-        let i = self.index(x, y);
+    fn fill(&mut self, x: usize, y: usize, transposed: bool) {
+        let i = self.index(x, y, transposed);
         self.filled.put(i);
     }
-    fn cross(&mut self, x: usize, y: usize) {
-        let i = self.index(x, y);
+    fn cross(&mut self, x: usize, y: usize, transposed: bool) {
+        let i = self.index(x, y, transposed);
         self.crossed.put(i);
     }
-    fn is_crossed(&self, x: usize, y: usize) -> bool {
-        let i = self.index(x, y);
+    fn is_crossed(&self, x: usize, y: usize, transposed: bool) -> bool {
+        let i = self.index(x, y, transposed);
         self.crossed.contains(i)
     }
-    fn is_filled(&self, x: usize, y: usize) -> bool {
-        let i = self.index(x, y);
+    fn is_filled(&self, x: usize, y: usize, transposed: bool) -> bool {
+        let i = self.index(x, y, transposed);
         self.filled.contains(i)
     }
 }
 
 struct Puzzle {
-    vert_clues: ClueList,
-    horz_clues: ClueList,
+    clues: Clues,
     grid: Grid,
 }
 
 impl Puzzle {
+    fn apply(&mut self, pass: &impl Pass) {
+        Pass::run(pass, self, true);
+        Pass::run(pass, self, false);
+    }
     fn try_from_ast(ast: parser::Puzzle) -> Result<Puzzle, String> {
         let w = ast.vert_clues.0.len();
         let h = ast.horz_clues.0.len();
@@ -99,10 +143,13 @@ impl Puzzle {
                 }
             }
             Ok(Puzzle {
-                vert_clues: ast.vert_clues,
-                horz_clues: ast.horz_clues,
+                clues: Clues {
+                    vert: ast.vert_clues,
+                    horz: ast.horz_clues,
+                },
                 grid: Grid {
                     width: w,
+                    height: h,
                     filled,
                     crossed,
                 },
@@ -111,10 +158,13 @@ impl Puzzle {
             let filled = FixedBitSet::with_capacity(w * h);
             let crossed = FixedBitSet::with_capacity(w * h);
             Ok(Puzzle {
-                vert_clues: ast.vert_clues,
-                horz_clues: ast.horz_clues,
+                clues: Clues {
+                    vert: ast.vert_clues,
+                    horz: ast.horz_clues,
+                },
                 grid: Grid {
                     width: w,
+                    height: h,
                     filled,
                     crossed,
                 },
@@ -123,57 +173,35 @@ impl Puzzle {
     }
     fn into_ast_without_grid(self) -> parser::Puzzle {
         parser::Puzzle {
-            horz_clues: self.horz_clues,
-            vert_clues: self.vert_clues,
+            horz_clues: self.clues.horz,
+            vert_clues: self.clues.vert,
             grid: None,
         }
     }
     fn into_ast(self) -> parser::Puzzle {
-        let h = self.horz_clues.0.len();
-        let w = self.vert_clues.0.len();
+        let h = self.clues.horz.0.len();
+        let w = self.clues.vert.0.len();
         let mut grid_lines = Vec::with_capacity(w);
         for y in 0..h {
-            let cells = (0..w).map(|x| self.grid.get(x, y)).collect();
+            let cells = (0..w).map(|x| self.grid.get(x, y, false)).collect();
             grid_lines.push(GridLine(cells));
         }
         parser::Puzzle {
-            horz_clues: self.horz_clues,
-            vert_clues: self.vert_clues,
+            horz_clues: self.clues.horz,
+            vert_clues: self.clues.vert,
             grid: Some(parser::Grid(grid_lines)),
         }
-    }
-    fn width(&self) -> usize {
-        self.vert_clues.0.len()
-    }
-    fn height(&self) -> usize {
-        self.horz_clues.0.len()
-    }
-    fn max_horz_clue_len(&self) -> usize {
-        self.horz_clues
-            .0
-            .iter()
-            .map(|clue| clue.0.len())
-            .max()
-            .unwrap()
-    }
-    fn max_vert_clue_len(&self) -> usize {
-        self.vert_clues
-            .0
-            .iter()
-            .map(|clue| clue.0.len())
-            .max()
-            .unwrap()
     }
 }
 
 impl fmt::Display for Puzzle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let w = self.vert_clues.0.len();
-        let max_vert_clue_len = self.max_vert_clue_len();
-        let max_horz_clue_len = self.max_horz_clue_len();
+        let w = self.clues.vert.0.len();
+        let max_vert_clue_len = self.clues.max_vert_len(false);
+        let max_horz_clue_len = self.clues.max_horz_len(false);
         for i in 0..max_vert_clue_len {
             write!(f, "{: >width$}", "", width = 3 * max_horz_clue_len)?;
-            for clue in &self.vert_clues.0 {
+            for clue in &self.clues.vert.0 {
                 if clue.0.len() > max_vert_clue_len - i - 1 {
                     write!(f, "{: >2}", clue.0[clue.0.len() - (max_vert_clue_len - i)])?;
                 } else {
@@ -182,7 +210,7 @@ impl fmt::Display for Puzzle {
             }
             write!(f, "\n")?;
         }
-        for (y, clue) in self.horz_clues.0.iter().enumerate() {
+        for (y, clue) in self.clues.horz.0.iter().enumerate() {
             for i in 0..max_horz_clue_len {
                 if clue.0.len() > max_horz_clue_len - i - 1 {
                     write!(f, " {: >2}", clue.0[clue.0.len() - (max_horz_clue_len - i)])?;
@@ -191,7 +219,7 @@ impl fmt::Display for Puzzle {
                 }
             }
             for x in 0..w {
-                write!(f, " {}", self.grid.get(x, y))?;
+                write!(f, " {}", self.grid.get(x, y, false))?;
             }
             write!(f, "\n")?;
         }
@@ -200,37 +228,24 @@ impl fmt::Display for Puzzle {
 }
 
 trait Pass {
-    fn run(puzzle: &mut Puzzle);
+    fn run(&self, puzzle: &mut Puzzle, transposed: bool);
 }
 
 struct BasicFreedom;
 
 impl Pass for BasicFreedom {
-    fn run(puzzle: &mut Puzzle) {
-        for (y, clue) in puzzle.horz_clues.0.iter().enumerate() {
+    fn run(&self, puzzle: &mut Puzzle, transposed: bool) {
+        for (y, clue) in puzzle.clues.horz.0.iter().enumerate() {
             let sum: usize = clue.0.iter().sum();
-            let freedom: usize = puzzle.width() - (sum + clue.0.len() - 1);
+            let freedom: usize = puzzle.clues.width(transposed) - (sum + clue.0.len() - 1);
             let mut x0 = 0;
             for number in clue.0.iter() {
                 if *number > freedom {
                     for x1 in x0 + freedom..x0 + number {
-                        puzzle.grid.fill(x1, y);
+                        puzzle.grid.fill(x1, y, transposed);
                     }
                 }
                 x0 += number + 1;
-            }
-        }
-        for (x, clue) in puzzle.vert_clues.0.iter().enumerate() {
-            let sum: usize = clue.0.iter().sum();
-            let freedom: usize = puzzle.height() - (sum + clue.0.len() - 1);
-            let mut y0 = 0;
-            for number in clue.0.iter() {
-                if *number > freedom {
-                    for y1 in y0 + freedom..y0 + number {
-                        puzzle.grid.fill(x, y1);
-                    }
-                }
-                y0 += number + 1;
             }
         }
     }
@@ -239,22 +254,26 @@ impl Pass for BasicFreedom {
 struct Freedom2;
 
 impl Pass for Freedom2 {
-    fn run(puzzle: &mut Puzzle) {
-        for (y, clue) in puzzle.horz_clues.0.iter().enumerate() {
+    fn run(&self, puzzle: &mut Puzzle, transposed: bool) {
+        for (y, clue) in puzzle.clues.horz(transposed).iter().enumerate() {
             let mut min_starts = Vec::with_capacity(clue.0.len());
             let mut x0 = 0;
             for number in clue.0.iter() {
                 let mut x = x0;
                 while x < x0 + number {
-                    if puzzle.grid.is_crossed(x, y) {
+                    if puzzle.grid.is_crossed(x, y, transposed) {
                         // pushing cross
                         x0 = x + 1;
                     }
                     x += 1;
                 }
-                if puzzle.grid.is_filled(x, y) {
+                if x + 1 < puzzle.clues.width(transposed)
+                    && puzzle.grid.is_filled(x + 1, y, transposed)
+                {
                     // pulling fill
-                    while puzzle.grid.is_filled(x, y) {
+                    while x + 1 < puzzle.clues.width(transposed)
+                        && puzzle.grid.is_filled(x + 1, y, transposed)
+                    {
                         x += 1;
                     }
                     // TODO check for impossibility
@@ -264,21 +283,21 @@ impl Pass for Freedom2 {
                 x0 = x0 + 1 + number;
             }
             let mut max_ends = Vec::with_capacity(clue.0.len());
-            let mut x1 = puzzle.width() + 1;
+            let mut x1 = puzzle.clues.width(transposed) + 1;
             for number in clue.0.iter().rev() {
                 x1 -= 1;
                 let mut x = x1 - 1;
                 while x + 1 < x1 - number {
-                    if puzzle.grid.is_crossed(x, y) {
+                    if puzzle.grid.is_crossed(x, y, transposed) {
                         // pushing cross
                         x1 = x;
                     }
                     x -= 1;
                 }
 
-                if puzzle.grid.is_filled(x, y) {
+                if x > 0 && puzzle.grid.is_filled(x, y, transposed) {
                     // pulling fill
-                    while puzzle.grid.is_filled(x, y) {
+                    while x > 0 && puzzle.grid.is_filled(x - 1, y, transposed) {
                         x -= 1;
                     }
                     // TODO check for impossibility
@@ -295,15 +314,15 @@ impl Pass for Freedom2 {
             {
                 if *max_end == *min_start + number {
                     if *min_start > 0 {
-                        puzzle.grid.cross(min_start - 1, y);
+                        puzzle.grid.cross(min_start - 1, y, transposed);
                     }
-                    if *max_end < puzzle.width() {
-                        puzzle.grid.cross(*max_end, y);
+                    if *max_end < puzzle.clues.width(transposed) {
+                        puzzle.grid.cross(*max_end, y, transposed);
                     }
                 }
                 if *max_end < *min_start + 2 * number {
                     for x in max_end - number..min_start + number {
-                        puzzle.grid.fill(x, y);
+                        puzzle.grid.fill(x, y, transposed);
                     }
                 }
             }
@@ -322,7 +341,7 @@ fn main() {
             .unwrap();
         match Puzzle::try_from_ast(ast) {
             Ok(mut puzzle) => {
-                Freedom2::run(&mut puzzle);
+                puzzle.apply(&Freedom2);
                 println!("{}", puzzle);
             }
             Err(e) => panic!("{}", e),
