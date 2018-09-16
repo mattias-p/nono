c2bc0ea066402ce4f139ae98caea862a552fc3ca
Mattias Päivärinta <mattias@paivarinta.se>
2018-09-16 19:37:58 +0200

    Refactor

---
+++ src/pass/crowded_clue.rs
@@ -0,0 +1,42 @@
+use puzzle::Line;
+use puzzle::LineHint;
+use puzzle::LinePass;
+
+#[derive(Debug)]
+struct CrowdedClueHint {
+    kernel_start: usize,
+    kernel_end: usize,
+}
+
+impl LineHint for CrowdedClueHint {
+    fn check(&self, line: &Line) -> bool {
+        line.range_contains_unfilled(self.kernel_start..self.kernel_end)
+    }
+    fn apply(&self, line: &mut Line) {
+        line.fill_range(self.kernel_start..self.kernel_end);
+    }
+}
+
+pub struct CrowdedCluePass;
+
+impl LinePass for CrowdedCluePass {
+    fn run(&self, clue: &[usize], line: &Line) -> Vec<Box<LineHint>> {
+        let mut hints: Vec<Box<LineHint>> = vec![];
+        let sum: usize = clue.iter().sum();
+        let freedom: usize = line.len() - (sum + clue.len() - 1);
+        let mut x0 = 0;
+        for number in clue.iter() {
+            if *number > freedom {
+                let hint = Box::new(CrowdedClueHint {
+                    kernel_start: x0 + freedom,
+                    kernel_end: x0 + number,
+                });
+                if hint.check(line) {
+                    hints.push(hint);
+                }
+            }
+            x0 += number + 1;
+        }
+        hints
+    }
+}
---
+++ src/pass/mod.rs
@@ -0,0 +1,5 @@
+mod continuous_range;
+mod crowded_clue;
+
+pub use pass::continuous_range::*;
+pub use pass::crowded_clue::*;
